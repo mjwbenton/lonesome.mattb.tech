c1164fd86cb72f57209ecbdccc4d016d1e114e5e
Matthew Benton <m@mattb.tech>
2017-08-07 23:02:18 -0700

    Use srcset and sizes for images

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
-import {Photo} from "staircase-generator/features";
+import {Photo as PhotoType, PhotoSource} from "staircase-generator/features";
 
-const Photo: React.SFC<Photo> = ({pageUrl, sources, mainSource, title}) => {
+function generateSrcSet(sources: PhotoSource[]): string {
+    return sources.map((source: PhotoSource) => [source.url, " ", source.width, "w"].join("")).join(", ");
+}
+
+const Photo: React.SFC<PhotoType> = ({pageUrl, sources, mainSource, title}) => {
     return <div className="mb-photo" >
-        <img className="mb-photo__image" src={mainSource.url}
-             alt={`Image titled "${title}"`}/>
+        <img className="mb-photo__image" src={mainSource.url} srcSet={generateSrcSet(sources)}
+             sizes="(max-width: 1090px) 100vw, 63vw" alt={`Image titled "${title}"`}/>
         <div className="mb-photo__overlay">
             <h3 className="mb-photo__overlay__title">{title}</h3>
             <a className="mb-photo__overlay__flickr-link" href={pageUrl}
