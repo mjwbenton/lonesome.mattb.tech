3614d9dbf2347c0263846230856b78f1a4494f42
Matthew Benton <m@mattb.tech>
2017-08-19 12:02:10 -0700

    Rename overlay to infoline

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -9,9 +9,9 @@ const Photo: React.SFC<PhotoType> = ({pageUrl, sources, mainSource, title}) => {
     return <div className="mb-photo" >
         <img className="mb-photo__image" src={mainSource.url} srcSet={generateSrcSet(sources)}
              sizes="(max-width: 1090px) 100vw, 63vw" alt={`Image titled "${title}"`}/>
-        <div className="mb-photo__overlay">
-            <h3 className="mb-photo__overlay__title">{title}</h3>
-            <a className="mb-photo__overlay__flickr-link" href={pageUrl}
+        <div className="mb-photo__infoline">
+            <h3 className="mb-photo__infoline__title">{title}</h3>
+            <a className="mb-photo__infoline__flickr-link" href={pageUrl}
                  title="Flickr Page">Fl</a>
         </div>
     </div>;
