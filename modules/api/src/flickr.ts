6ce0355f8cd950239e5822f70b86e65898326547
Matthew Benton <m@mattb.tech>
2018-09-10 08:49:13 +0100

    Updates to API code, trying to get caching working

--- modules/api/src/flickr.ts
+++ modules/api/src/flickr.ts
@@ -3,7 +3,7 @@ import * as request from 'request-promise-native';
 const FLICKR_API_BASE_URL = 'https://api.flickr.com/services/rest/';
 const FLICKR_BASE_PARAMETERS = '?format=json&nojsoncallback=1';
 
-export default async function callFlickr(apiKey : string, methodName : string,
+export async function callFlickr(apiKey : string, methodName : string,
                           params : { [key : string] : string }, retryNumber: number = 0): Promise<any> {
     let url = FLICKR_API_BASE_URL + FLICKR_BASE_PARAMETERS
         + `&api_key=${apiKey}&`;
@@ -14,7 +14,6 @@ export default async function callFlickr(apiKey : string, methodName : string,
     });
     url += paramsStr;
     try {
-        console.log(url);
         return request(url).then(JSON.parse);
     } catch (err) {
         if (retryNumber < 2) {
@@ -23,3 +22,52 @@ export default async function callFlickr(apiKey : string, methodName : string,
         throw err;
     }
 }
+
+interface PhotoSource {
+    url: string,
+    width: number,
+    height: number,
+}
+
+interface RecentPhoto {
+    pageUrl: string,
+    title: string,
+    mainSource: PhotoSource,
+    sources: PhotoSource[]
+}
+
+const FLICKR_URL_BASE = "https://www.flickr.com/photos/";
+
+export async function getRecentPhotos(apiKey: string, user_id: string): Promise<RecentPhoto[]> {
+    const response = await callFlickr(process.env.FLICKR_API_KEY, "flickr.people.getPublicPhotos", {
+        user_id: "83914470@N00",
+        extras: "url_z, url_c, url_l, url_k",
+        per_page: "50"
+    });
+    return response.photos.photo.map((p) => ({
+        pageUrl: `${FLICKR_URL_BASE}${p.owner}/${p.id}/`,
+        title: p.title,
+        mainSource: {
+            url: p.url_c,
+            height: p.height_c,
+            width: p.width_c
+        },
+        sources: buildSources(p)
+    }));
+}
+
+function buildSources(photoResponse: any): PhotoSource[] {
+    const result: PhotoSource[] = [];
+    Object.keys(photoResponse).forEach((key) => {
+        if (key.startsWith("url_")) {
+            const sizeKey = key.replace("url_", "");
+            result.push({
+                url: photoResponse[key],
+                height: photoResponse[`height_${sizeKey}`],
+                width: photoResponse[`width_${sizeKey}`],
+            });
+        }
+    });
+    return result;
+}
+
--- modules/api/src/index.ts
+++ modules/api/src/index.ts
@@ -1,11 +1,12 @@
-import callFlickr from "./flickr";
+import {callFlickr, getRecentPhotos} from "./flickr";
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 export async function handler(event: any, context: any, callback: any) {
-    console.log(process.env.FLICKR_API_KEY);
-    callback(null, await callFlickr(process.env.FLICKR_API_KEY, "flickr.people.getPublicPhotos", {
-        "user_id": "83914470@N00"
-    }));
+    const response = await getRecentPhotos(process.env.FLICKR_API_KEY, "83914470@N00");
+    callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(response)
+    });
 }
