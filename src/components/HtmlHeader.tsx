6984d1873efec2d591fa2bbd12e113a7c313060f
Matthew Benton <m@mattb.tech>
2018-11-18 18:28:04 +0000

    Update title

--- src/components/HtmlHeader.tsx
+++ src/components/HtmlHeader.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 const HtmlHeader: React.SFC<{}> = () => {
     return <head>
-        <title>Matthew Benton Photographer</title>
+        <title>mattb.tech</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="/style.css" />
         <link rel="stylesheet" href="https://use.typekit.net/ukk8ctr.css" />
