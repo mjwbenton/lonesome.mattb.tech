6a423c51334beae3fe6a1f7f043bbd19fde70c43
Matthew Benton <m@mattb.tech>
2019-01-01 13:51:55 +0000

    Remove unused jsExtensionPoint

--- src/components/RightSide.tsx
+++ src/components/RightSide.tsx
@@ -5,7 +5,6 @@ const RightSide: React.SFC<{contentHtml: string, photos: React.ReactChild}> = ({
         <div className="mb-content"
              dangerouslySetInnerHTML={{ __html: contentHtml }} />
         {photos}
-        <div id="jsExtensionPoint"></div>
     </div>;
 };
 export default RightSide;
