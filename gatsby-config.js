ce65d9eaf3259b2751460c8623f06a0f2fd1373d
Matthew Benton <m@mattb.tech>
2019-01-01 13:33:05 +0000

    Add back accidentally ignored config file

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: { path: `${__dirname}/content`, name: "markdown-pages" }
+    },
+    `@mattb/gatsby-transform-flickr-set`,
+    `@mattb/gatsby-transform-spotify-playlist`
+  ]
+};
