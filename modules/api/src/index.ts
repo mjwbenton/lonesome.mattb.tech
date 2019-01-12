6535e479804c7027ad14d4e63d8d5fb87635df43
Matthew Benton <m@mattb.tech>
2019-01-12 23:29:27 +0000

    Use @mattb/flickr-api

--- modules/api/src/index.ts
+++ modules/api/src/index.ts
@@ -1,16 +1,19 @@
-import {callFlickr, getRecentPhotos} from "./flickr";
-import * as dotenv from 'dotenv';
+import { getRecentPhotos } from "@mattb/flickr-api";
+import * as dotenv from "dotenv";
 import renderHtml from "./template";
 
 dotenv.config();
 
 export async function handler(event: any, context: any, callback: any) {
-    const response = await getRecentPhotos(process.env.FLICKR_API_KEY, "83914470@N00");
-    callback(null, {
-        statusCode: 200,
-        body: renderHtml(response),
-        headers: {
-            "Access-Control-Allow-Origin": "*"
-        }
-    });
+  const response = await getRecentPhotos(
+    process.env.FLICKR_API_KEY,
+    "83914470@N00"
+  );
+  callback(null, {
+    statusCode: 200,
+    body: renderHtml(response),
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    }
+  });
 }
