d62a4f8e0ab06cfbab51b19b5b658021532ec1d0
Matthew Benton <m@mattb.tech>
2019-01-13 09:53:43 +0000

    Use React components to show stream photos

--- src/components/Stream.tsx
+++ src/components/Stream.tsx
@@ -1,23 +1,36 @@
 import React from "react";
+import { Photo } from "@mattb/gatsby-transform-flickr-set";
+import SinglePhoto from "./Photo";
 
-export default class Stream extends React.Component<{}, { html: string }> {
+export default class Stream extends React.Component<
+  {},
+  { photos: Array<Photo> | null }
+> {
   constructor(props) {
     super(props);
     this.state = {
-      html: "Loading..."
+      photos: null
     };
   }
   componentDidMount() {
     fetch("https://umpghq4xo2.execute-api.us-east-1.amazonaws.com/Prod/photos")
-      .then(response => response.text())
-      .then(html => this.setState({ html }));
+      .then(response => response.json())
+      .then(photos => this.setState({ photos }));
   }
   render() {
+    if (this.state.photos == null) {
+      return "Loading...";
+    }
     return (
-      <div
-        className="mb-stream"
-        dangerouslySetInnerHTML={{ __html: this.state.html }}
-      />
+      <div className="mb-photos">
+        <ul>
+          {this.state.photos.map(p => (
+            <li key={p.pageUrl}>
+              <SinglePhoto {...p} />
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
