818686d2860a9bccd434b43f05d3b272c607d4ae
Matthew Benton <m@mattb.tech>
2019-03-17 14:37:40 +0000

    Use isomorphic fetch for when apollo client is invoked during ssr

--- src/global/ApiProvider.tsx
+++ src/global/ApiProvider.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { ApolloProvider } from "react-apollo-hooks";
 import ApolloClient from "apollo-boost";
+import fetch from "isomorphic-fetch";
 
 const ENDPOINT =
   "https://umpghq4xo2.execute-api.us-east-1.amazonaws.com/Prod/graphql";
 
 const client = new ApolloClient({
-  uri: ENDPOINT
+  uri: ENDPOINT,
+  fetch
 });
 
 export default ({ children }: { children: any }) => (
