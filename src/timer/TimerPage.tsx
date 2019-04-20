4eb54f23a8511274f5ece2fa01ba30fe08e981b2
Matthew Benton <m@mattb.tech>
2019-04-20 13:41:21 +0100

    Add fp3000b

--- src/timer/TimerPage.tsx
+++ src/timer/TimerPage.tsx
@@ -33,38 +33,62 @@ const TimerTableRow = styled.tr`
   }
 `;
 
-const fp100c = [
-  {
-    c: 10,
-    f: 50,
-    t: 270
-  },
-  {
-    c: 15,
-    f: 59,
-    t: 180
-  },
-  {
-    c: 20,
-    f: 68,
-    t: 120
-  },
-  {
-    c: 25,
-    f: 77,
-    t: 90
-  },
-  {
-    c: 30,
-    f: 86,
-    t: 75
-  },
-  {
-    c: 35,
-    f: 95,
-    t: 60
-  }
-];
+const timings = {
+  fp100c: [
+    {
+      c: 10,
+      f: 50,
+      t: 270
+    },
+    {
+      c: 15,
+      f: 59,
+      t: 180
+    },
+    {
+      c: 20,
+      f: 68,
+      t: 120
+    },
+    {
+      c: 25,
+      f: 77,
+      t: 90
+    },
+    {
+      c: 30,
+      f: 86,
+      t: 75
+    },
+    {
+      c: 35,
+      f: 95,
+      t: 60
+    }
+  ],
+  fp3000b: [
+    {
+      c: "15 - 17",
+      f: "59 - 63",
+      t: 30
+    },
+    {
+      c: "18 - 19",
+      f: "64 - 66",
+      t: 25
+    },
+    {
+      c: "20 - 23",
+      f: "67 - 73",
+      t: 20
+    },
+    {
+      c: "24 - 35",
+      f: "74 - 95",
+      t: 15
+    }
+  ]
+};
 
 const TimerPage = () => {
   const [timerDuration, setTimerDuration] = useState(0);
@@ -73,44 +97,50 @@ const TimerPage = () => {
   );
   return (
     <Layout>
-      <H2>fp100c</H2>
       <Timer {...{ minutes, seconds, state, startStop }} />
-      <TimerTable>
-        <tbody>
-          <TimerTableRow>
-            <TimerHeader>
-              <Thermometer />
-              Temp (°C)
-            </TimerHeader>
-            <TimerHeader>
-              <Thermometer />
-              Temp (°F)
-            </TimerHeader>
-            <TimerHeader>
-              <Watch />
-              Dev Time (seconds)
-            </TimerHeader>
-            <TimerHeader />
-          </TimerTableRow>
-          {fp100c.map(({ c, f, t }) => (
-            <TimerTableRow key={c}>
-              <TimerData>{c}</TimerData>
-              <TimerData>{f}</TimerData>
-              <TimerData>{t}</TimerData>
-              <TimerData>
-                <a
-                  href="#"
-                  onClick={() => {
-                    state !== State.RUNNING && setTimerDuration(t);
-                  }}
-                >
-                  <Watch color={state !== State.RUNNING ? "black" : "grey"} />
-                </a>
-              </TimerData>
-            </TimerTableRow>
-          ))}
-        </tbody>
-      </TimerTable>
+      {Object.keys(timings).map(film => (
+        <div>
+          <H2>{film}</H2>
+          <TimerTable>
+            <tbody>
+              <TimerTableRow>
+                <TimerHeader>
+                  <Thermometer />
+                  Temp (°C)
+                </TimerHeader>
+                <TimerHeader>
+                  <Thermometer />
+                  Temp (°F)
+                </TimerHeader>
+                <TimerHeader>
+                  <Watch />
+                  Dev Time (seconds)
+                </TimerHeader>
+                <TimerHeader />
+              </TimerTableRow>
+              {timings[film].map(({ c, f, t }) => (
+                <TimerTableRow key={c}>
+                  <TimerData>{c}</TimerData>
+                  <TimerData>{f}</TimerData>
+                  <TimerData>{t}</TimerData>
+                  <TimerData>
+                    <a
+                      href="#"
+                      onClick={() => {
+                        state !== State.RUNNING && setTimerDuration(t);
+                      }}
+                    >
+                      <Watch
+                        color={state !== State.RUNNING ? "black" : "grey"}
+                      />
+                    </a>
+                  </TimerData>
+                </TimerTableRow>
+              ))}
+            </tbody>
+          </TimerTable>
+        </div>
+      ))}
     </Layout>
   );
 };
