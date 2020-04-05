245c6e85674d5d7a6bdd4e2e9de0d0a5a0b2e88b
Matthew Benton <m@mattb.tech>
2020-04-05 15:25:32 +0100

    Use DNS validated certificates

--- cdk/stack.ts
+++ cdk/stack.ts
@@ -1,46 +1,35 @@
+import path from "path";
 import * as cdk from "@aws-cdk/core";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as route53 from "@aws-cdk/aws-route53";
 import * as route53targets from "@aws-cdk/aws-route53-targets";
 import * as cloudfront from "@aws-cdk/aws-cloudfront";
 import * as acm from "@aws-cdk/aws-certificatemanager";
+import * as s3deploy from "@aws-cdk/aws-s3-deployment";
+import WebsiteRedirect from "./WebsiteRedirect";
 
-const DOMAIN_NAME = {
-  domainName: "mattb.tech",
-  hostedZone: "mattb.tech",
-  hostedZoneId: "Z2GPSB1CDK86DH"
-};
+const ZONE_NAME = "mattb.tech";
+const DOMAIN_NAME = "mattb.tech";
+const HOSTED_ZONE_ID = "Z2GPSB1CDK86DH";
 
 const REDIRECT_DOMAIN_NAMES = [
   {
-    domainName: "www.mattb.tech",
     hostedZone: "mattb.tech",
-    hostedZoneId: "Z2GPSB1CDK86DH"
+    hostedZoneId: "Z2GPSB1CDK86DH",
+    domainName: "www.mattb.tech",
+    alternateNames: []
   },
   {
+    hostedZone: "mattbenton.co.uk",
+    hostedZoneId: "Z37GS1FXPT1S5S",
     domainName: "mattbenton.co.uk",
-    hostedZone: "mattbenton.co.uk",
-    hostedZoneId: "Z37GS1FXPT1S5S"
+    alternateNames: ["www.mattbenton.co.uk", "blog.mattbenton.co.uk"]
   },
   {
-    domainName: "www.mattbenton.co.uk",
-    hostedZone: "mattbenton.co.uk",
-    hostedZoneId: "Z37GS1FXPT1S5S"
-  },
-  {
-    domainName: "blog.mattbenton.co.uk",
-    hostedZone: "mattbenton.co.uk",
-    hostedZoneId: "Z37GS1FXPT1S5S"
-  },
-  {
+    hostedZone: "lionsmane.co.uk",
+    hostedZoneId: "ZNKR9NWXWS7UU",
     domainName: "lionsmane.co.uk",
-    hostedZone: "lionsmane.co.uk",
-    hostedZoneId: "ZNKR9NWXWS7UU"
-  },
-  {
-    domainName: "www.lionsmane.co.uk",
-    hostedZone: "lionsmane.co.uk",
-    hostedZoneId: "ZNKR9NWXWS7UU"
+    alternateNames: ["www.lionsmane.co.uk"]
   }
 ];
 
@@ -48,15 +37,24 @@ export class MattbTechWebsite extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const mainHostedZone = route53.HostedZone.fromHostedZoneAttributes(
+      this,
+      "HostedZone",
+      {
+        hostedZoneId: HOSTED_ZONE_ID,
+        zoneName: ZONE_NAME
+      }
+    );
+
     const siteBucket = new s3.Bucket(this, "SiteBucket", {
       accessControl: s3.BucketAccessControl.PUBLIC_READ,
-      websiteIndexDocument: "index.html"
+      websiteIndexDocument: "index.html",
+      publicReadAccess: true
     });
 
-    const certificate = new acm.Certificate(this, "Certificate", {
-      domainName: DOMAIN_NAME.domainName,
-      subjectAlternativeNames: REDIRECT_DOMAIN_NAMES.map(dn => dn.domainName),
-      validationMethod: acm.ValidationMethod.DNS
+    const certificate = new acm.DnsValidatedCertificate(this, "Certificate", {
+      domainName: DOMAIN_NAME,
+      hostedZone: mainHostedZone
     });
 
     const distribution = new cloudfront.CloudFrontWebDistribution(
@@ -85,86 +83,44 @@ export class MattbTechWebsite extends cdk.Stack {
         viewerCertificate: cloudfront.ViewerCertificate.fromAcmCertificate(
           certificate,
           {
-            aliases: [DOMAIN_NAME.domainName]
+            aliases: [DOMAIN_NAME]
           }
         ),
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
       }
     );
 
-    const redirectBucket = new s3.Bucket(this, "RedirectBucket", {
-      accessControl: s3.BucketAccessControl.PUBLIC_READ,
-      websiteRedirect: {
-        hostName: DOMAIN_NAME.domainName,
-        protocol: s3.RedirectProtocol.HTTPS
-      }
+    new s3deploy.BucketDeployment(this, "DeploySite", {
+      sources: [s3deploy.Source.asset(path.join(__dirname, "../public"))],
+      destinationBucket: siteBucket,
+      distribution
     });
 
-    const redirectDistribution = new cloudfront.CloudFrontWebDistribution(
-      this,
-      "RedirectDistribution",
-      {
-        originConfigs: [
-          {
-            behaviors: [
-              {
-                isDefaultBehavior: true,
-                defaultTtl: cdk.Duration.minutes(5),
-                compress: true,
-                allowedMethods: cloudfront.CloudFrontAllowedMethods.GET_HEAD,
-                forwardedValues: {
-                  queryString: false
-                }
-              }
-            ],
-            customOriginSource: {
-              domainName: redirectBucket.bucketWebsiteDomainName,
-              originProtocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY
-            }
-          }
-        ],
-        viewerCertificate: cloudfront.ViewerCertificate.fromAcmCertificate(
-          certificate,
-          {
-            aliases: REDIRECT_DOMAIN_NAMES.map(dn => dn.domainName)
-          }
-        ),
-        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
-      }
-    );
-
     new route53.ARecord(this, "DomainRecord", {
-      zone: route53.HostedZone.fromHostedZoneAttributes(
-        this,
-        "DomainRecordZone",
-        {
-          hostedZoneId: DOMAIN_NAME.hostedZoneId,
-          zoneName: DOMAIN_NAME.hostedZone
-        }
-      ),
-      recordName: DOMAIN_NAME.domainName,
+      zone: mainHostedZone,
+      recordName: DOMAIN_NAME,
       ttl: cdk.Duration.minutes(5),
       target: route53.RecordTarget.fromAlias(
         new route53targets.CloudFrontTarget(distribution)
       )
     });
 
-    REDIRECT_DOMAIN_NAMES.forEach((domainName, i) => {
-      new route53.ARecord(this, `RedirectDomainRecord${i}`, {
-        zone: route53.HostedZone.fromHostedZoneAttributes(
-          this,
-          `RedirectDomainRecordZone${i}`,
-          {
-            hostedZoneId: domainName.hostedZoneId,
-            zoneName: domainName.hostedZone
-          }
-        ),
-        recordName: domainName.domainName,
-        ttl: cdk.Duration.minutes(5),
-        target: route53.RecordTarget.fromAlias(
-          new route53targets.CloudFrontTarget(redirectDistribution)
-        )
-      });
-    });
+    REDIRECT_DOMAIN_NAMES.forEach(
+      ({ hostedZone, hostedZoneId, domainName, alternateNames }) => {
+        new WebsiteRedirect(this, `RedirectFor${domainName}`, {
+          redirectTo: DOMAIN_NAME,
+          domainName: domainName,
+          alternateNames: alternateNames,
+          hostedZone: route53.HostedZone.fromHostedZoneAttributes(
+            this,
+            `HostedZone${hostedZone}`,
+            {
+              hostedZoneId,
+              zoneName: hostedZone
+            }
+          )
+        });
+      }
+    );
   }
 }
