e8606d29f15d5489bec6eb8046f364c9b77412c3
Matthew Benton <m@mattb.tech>
2020-04-18 18:17:15 +0100

    Enable persisted queries

--- src/global/ApiProvider.tsx
+++ src/global/ApiProvider.tsx
@@ -3,30 +3,33 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import fetch from "isomorphic-fetch";
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { createPersistedQueryLink } from "apollo-link-persisted-queries";
 import { HttpLink } from "apollo-link-http";
 import { onError } from "apollo-link-error";
 import { ApolloLink } from "apollo-link";
 
-const ENDPOINT = "https://api.mattb.tech";
+const ENDPOINT = "https://api.mattb.tech/";
 
 const client = new ApolloClient({
-  link: ApolloLink.from([
-    onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors)
-        graphQLErrors.forEach(({ message, locations, path }) =>
-          console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-          )
-        );
-      if (networkError) console.log(`[Network error]: ${networkError}`);
-    }),
-    new HttpLink({
-      uri: ENDPOINT,
-      credentials: "same-origin",
-      fetch,
-      useGETForQueries: true
-    })
-  ]),
+  link: createPersistedQueryLink().concat(
+    ApolloLink.from([
+      onError(({ graphQLErrors, networkError }) => {
+        if (graphQLErrors)
+          graphQLErrors.forEach(({ message, locations, path }) =>
+            console.log(
+              `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+            )
+          );
+        if (networkError) console.log(`[Network error]: ${networkError}`);
+      }),
+      new HttpLink({
+        uri: ENDPOINT,
+        credentials: "same-origin",
+        fetch,
+        useGETForQueries: true
+      })
+    ])
+  ),
   cache: new InMemoryCache()
 });
 
