15001e61989f4d5dd93f1284a8991a5730b86793
Matthew Benton <m@mattb.tech>
2020-12-16 14:53:22 +0000

    move global styles to postcss/tailwind

--- src/global/Layout.tsx
+++ src/global/Layout.tsx
@@ -1,22 +1,15 @@
 import React from "react";
-import styled from "styled-components";
 import HtmlHeader from "./HtmlHeader";
-import GlobalStyles from "../style/GlobalStyles";
 import Logo from "./Logo";
 import Navigation from "../navigation/Navigation";
 import ApiProvider from "./ApiProvider";
 
-const Content = styled.div`
-  margin: 2vw;
-`;
-
 const Layout: React.FunctionComponent<{ children: any }> = ({ children }) => (
   <ApiProvider>
-    <GlobalStyles />
     <HtmlHeader />
     <Logo />
     <Navigation />
-    <Content>{children}</Content>
+    <div className="m-4">{children}</div>
   </ApiProvider>
 );
 
