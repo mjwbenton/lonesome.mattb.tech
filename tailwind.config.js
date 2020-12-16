7624f3edd5360bb6a90edb70d75c27c1f59470b5
Matthew Benton <m@mattb.tech>
2020-12-16 14:53:32 +0000

    Check in missing config files

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./src/**/*.tsx'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      'body': ["fira-sans", "Arial", "sans-serif"],
+      'heading': ["fira-sans-2", "Arial", "sans-serif"]
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
