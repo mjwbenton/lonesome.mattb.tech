9c4a0624c4dcaccc26aeca99c2f10b9e1fdf011a
Matthew Benton <m@mattb.tech>
2020-12-20 16:58:16 +0000

    TwoRowText

--- src/component/TwoRowText.tsx
+++ src/component/TwoRowText.tsx
@@ -1,20 +1,8 @@
 import React from "react";
-import styled from "styled-components";
-
-const Row1 = styled.h3`
-  font-size: 1rem;
-  font-weight: 700;
-`;
-
-const Row2 = styled.span`
-  font-style: italic;
-`;
-
-const Box = styled.div``;
 
 export default ({ row1, row2 }: { row1: string; row2: string }) => (
-  <Box>
-    <Row1>{row1}</Row1>
-    <Row2>{row2}</Row2>
-  </Box>
+  <div>
+    <h3 className="font-bold">{row1}</h3>
+    <span className="italic">{row2}</span>
+  </div>
 );
