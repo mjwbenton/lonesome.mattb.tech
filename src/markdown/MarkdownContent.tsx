bb46dcf18442b6f736a47c6b4277058d0e4b0e2f
Matthew Benton <m@mattb.tech>
2020-12-20 12:53:16 +0000

    Markdown content

--- src/markdown/MarkdownContent.tsx
+++ src/markdown/MarkdownContent.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import styled from "styled-components";
 import renderAst from "./renderAst";
-import { spacingUnit } from "../style/style";
 
 const MarkdownContent: React.FunctionComponent<{ htmlAst: any }> = ({
-  htmlAst
+  htmlAst,
 }) => <div>{renderAst(htmlAst)}</div>;
 
 export default MarkdownContent;
