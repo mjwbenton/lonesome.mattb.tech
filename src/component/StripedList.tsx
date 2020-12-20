e8e876412d228ecfbabc58aed9520e1b5b039ff6
Matthew Benton <m@mattb.tech>
2020-12-20 20:54:39 +0000

    Fix z-index issue with playlist

--- src/component/StripedList.tsx
+++ src/component/StripedList.tsx
@@ -7,9 +7,9 @@ export function StripedList(props: JSX.IntrinsicElements["ul"]) {
 export default StripedList;
 
 export const StripeElement = ({ children, index }) => (
-  <li className="relative z-10 flex p-4 overflow-hidden bg-gray-100 striped-list-element odd:bg-white space-x-4">
+  <li className="relative flex p-4 overflow-hidden bg-gray-100 striped-list-element odd:bg-white space-x-4">
     {children}
-    <span className="absolute top-0 left-0 z-0 w-full pr-2 -mt-6 text-right striped-index text-9xl">
+    <span className="absolute top-0 left-0 w-full pr-2 -mt-6 text-right striped-index text-9xl">
       {index}
     </span>
   </li>
