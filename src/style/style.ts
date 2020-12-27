d68e0d48219ff506a5b5e92a0f1907c53072609b
Matthew Benton <m@mattb.tech>
2020-12-27 19:23:29 +0000

    Remove unused style.ts

--- src/style/style.ts
+++
@@ -1,9 +0,0 @@
-export const spacingUnit = "1rem";
-export const halfSpacingUnit = "0.5rem";
-export const doubleSpacingUnit = "2rem";
-export const maxContentWidth = "45rem";
-export const fontColor = "#222";
-export const backgroundColor = "#fff";
-export const altBackgroundColor = "#f7f7f7"; // bg-grey-100
-export const fontStack = `"fira-sans", Arial, sans-serif`;
-export const fontStackHeading = `"fira-sans-2", Arial, sans-serif`;
