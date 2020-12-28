a1250838ac565dc8fe54773653de3fc61cbbffe2
Matthew Benton <m@mattb.tech>
2020-12-28 21:04:13 +0000

    More contrast behind logo

--- src/global/Logo.tsx
+++ src/global/Logo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default () => (
   <a
-    className="block w-full p-8 text-5xl italic text-center text-green-500 underline bg-gray-100 md:text-left font-heading"
+    className="block w-full p-8 text-5xl italic text-center text-green-500 underline md:text-left font-heading"
     href="/"
   >
     mattb
