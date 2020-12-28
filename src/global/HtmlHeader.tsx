bc6309261a608b66514f8f8091c66a2c98473f8e
Matthew Benton <m@mattb.tech>
2020-12-28 21:01:21 +0000

    Set html lang

--- src/global/HtmlHeader.tsx
+++ src/global/HtmlHeader.tsx
@@ -4,6 +4,7 @@ import { Helmet } from "react-helmet";
 const HtmlHeader: React.FunctionComponent<{}> = () => {
   return (
     <Helmet>
+      <html lang="en" />
       <title>mattb.tech</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="stylesheet" href="https://use.typekit.net/ukk8ctr.css" />
