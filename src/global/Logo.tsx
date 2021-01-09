c80cfef706ed279470b62b4c58d081db2a17210d
Matthew Benton <m@mattb.tech>
2021-01-09 19:45:29 +0000

    Take the logo out of the tabindex

--- src/global/Logo.tsx
+++ src/global/Logo.tsx
@@ -3,6 +3,7 @@ import React from "react";
 export default () => (
   <a
     className="block w-full p-8 text-5xl italic font-thin text-center text-green-700 underline underline-green-500 md:text-left"
+    tabIndex={-1}
     href="/"
   >
     &nbsp;mattb&nbsp;
