4a01893bbe6b96e0b2bdee09f948890b4fe21be9
Matthew Benton <m@mattb.tech>
2021-01-26 20:39:03 +0000

    Some basic MDX setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
-module.exports = {
+const detectFrontmatter = require("remark-frontmatter");
+const extractFrontmatter = require("remark-extract-frontmatter");
+const yaml = require("yaml").parse;
+
+const withMDX = require("@next/mdx")({
+  options: {
+    remarkPlugins: [
+      detectFrontmatter,
+      [extractFrontmatter, { name: "frontmatter", yaml, throws: true }],
+    ]
+  }
+});
+module.exports = withMDX({
+  pageExtensions: ["tsx", "mdx"],
   i18n: {
     locales: ['en'],
-    defaultLocale: 'en'
+    defaultLocale: 'en',
   }
-}
+})
