f6966299260dc7e2390d3500c8d75a4554316421
Matthew Benton <m@mattb.tech>
2021-01-31 21:32:17 +0000

    Fix tailwind purge configuration

--- packages/website/tailwind.config.js
+++ packages/website/tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   purge: {
     enabled: true,
-    content: ['./src/**/*.tsx'],
+    content: ['./src/**/*.tsx', './pages/**/*.{tsx,mdx}'],
     options: {
       keyframes: true
     }
