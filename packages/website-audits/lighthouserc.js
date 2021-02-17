59791116b7dad065c758471572247b08f64f0a2c
Matthew Benton <m@mattb.tech>
2021-02-17 18:26:33 +0000

    Make more things that currently exist on the site warnings in lighthouse

--- packages/website-audits/lighthouserc.js
+++ packages/website-audits/lighthouserc.js
@@ -11,9 +11,14 @@ module.exports = {
     assert: {
       preset: "lighthouse:no-pwa",
       assertions: {
-        "unused-javascript": "warn",
-        "unsized-images": "warn",
+        "image-aspect-ratio": "warn",
+        "image-size-responsive": "warn",
         "meta-description": "warn",
+        "offscreen-images": "warn",
+        "unsized-images": "warn",
+        "unused-javascript": "warn",
+        "uses-optimized-images": "warn",
+        "uses-responsive-images": "warn",
       },
     },
     upload: {
