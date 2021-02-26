539690ace44d87ffc61f5fa0f913d69f0a1eb091
Matthew Benton <m@mattb.tech>
2021-02-26 15:26:18 +0000

    Add favicon

--- packages/website/pages/_app.tsx
+++ packages/website/pages/_app.tsx
@@ -19,6 +19,7 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <title>{title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <ThemeProvider attribute="class">
         <header>
