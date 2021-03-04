a1c29c38d55bfce9c7dfee1472612746b1e8344b
Matthew Benton <m@mattb.tech>
2021-03-04 16:08:50 +0000

    Improve h1 tags on set and tag pages slightly

---
+++ packages/website/src/global/ShowQueryArg.tsx
@@ -0,0 +1,6 @@
+import { useRouter } from "next/router";
+
+export default function ShowQueryArg({ arg }: { arg: string }): string | null {
+  const router = useRouter();
+  return (router.query[arg] as string) ?? null;
+}
