63d9256b01517040c9a31218ef591c09cc7f02a9
Matthew Benton <m@mattb.tech>
2021-03-04 14:26:45 +0000

    Add types to photoSetDataProvider

--- packages/website/src/photo/photoSetDataProvider.ts
+++ packages/website/src/photo/photoSetDataProvider.ts
@@ -1,6 +1,7 @@
 import client from "../global/client";
 import gql from "graphql-tag";
 import { fragment } from "./Photo";
+import { PhotoSetQuery } from "generated/graphql";
 
 const QUERY = gql`
   query PhotoSet($photosetId: ID!) {
@@ -11,11 +12,13 @@ const QUERY = gql`
   ${fragment}
 `;
 
-export default async function photoSetDataProvider({ photosetId }) {
+export default async function photoSetDataProvider({
+  photosetId,
+}): Promise<PhotoSetQuery> {
   if (!photosetId) {
     throw new Error("Must provide photosetId");
   }
-  const result = await client.query({
+  const result = await client.query<PhotoSetQuery>({
     query: QUERY,
     variables: { photosetId },
   });
