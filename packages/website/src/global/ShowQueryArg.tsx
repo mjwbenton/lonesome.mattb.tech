3b60c97b5f2e8466212af86ae392b68a70d9d4a9
Matthew Benton <m@mattb.tech>
2021-04-06 16:51:13 +0100

    Handle router undefined

--- packages/website/src/global/ShowQueryArg.tsx
+++ packages/website/src/global/ShowQueryArg.tsx
@@ -2,5 +2,5 @@ import { useRouter } from "next/router";
 
 export default function ShowQueryArg({ arg }: { arg: string }): string | null {
   const router = useRouter();
-  return (router.query[arg] as string) ?? null;
+  return (router?.query[arg] as string) ?? null;
 }
