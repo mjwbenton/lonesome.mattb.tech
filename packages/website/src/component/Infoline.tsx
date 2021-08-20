1c45dd2fa9e73d56f6283eacc26e89c93f627245
Matthew Benton <m@mattb.tech>
2021-08-20 15:20:32 +0100

    Support infoline without link

--- packages/website/src/component/Infoline.tsx
+++ packages/website/src/component/Infoline.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 
-const Infoline = ({ externalLinkUrl, externalLinkText, children }) => (
+const Infoline = ({
+  externalLinkUrl,
+  externalLinkText,
+  children,
+}: {
+  externalLinkUrl?: string;
+  externalLinkText?: string;
+  children: any;
+}) => (
   <div className="flex p-2 text-xs bg-light-1 dark:bg-dark-1">
     <div className="flex-1 inline">{children}</div>
-    <a className="external-link" href={externalLinkUrl}>
-      {externalLinkText}
-    </a>
+    {externalLinkUrl ? (
+      <a className="external-link" href={externalLinkUrl}>
+        {externalLinkText}
+      </a>
+    ) : null}
   </div>
 );
 
