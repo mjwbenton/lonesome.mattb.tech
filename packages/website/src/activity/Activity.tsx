9a70f4a206bc3502cd300ad9eeadcb7ae18514bf
Matthew Benton <m@mattb.tech>
2023-01-18 16:54:45 +0000

    v2 of activity data on homepage

--- packages/website/src/activity/Activity.tsx
+++ packages/website/src/activity/Activity.tsx
@@ -11,11 +11,40 @@ export default function Activity() {
       <TopRightSpinner show={loading} />
       <Wall>
         <Tile>
-          <strong>{activity?.walkingRunningDistance.km.toFixed(2)}</strong>km
-          walked
+          <strong>{activity?.trailing30.walkingRunningKm}</strong> walked in the
+          last 30 days
+          <br />
+          <span className="text-xs">
+            {activity?.trailing30.walkingRunningPercentageChange} change on the
+            same time last year
+          </span>
         </Tile>
         <Tile>
-          <strong>{activity?.swimmingDistance.km.toFixed(2)}</strong>km swam
+          <strong>{activity?.trailing30.swimmingDistanceKm}</strong> swam in the
+          last 30 days
+          <br />
+          <span className="text-xs">
+            {activity?.trailing30.swimmingDistancePercentageChange} change on
+            the same time last year
+          </span>
+        </Tile>
+        <Tile>
+          <strong>{activity?.year.walkingRunningKm}</strong> walked so far this
+          year
+          <br />
+          <span className="text-xs">
+            {activity?.year.walkingRunningPercentageChange} change on the same
+            time last year
+          </span>
+        </Tile>
+        <Tile>
+          <strong>{activity?.year.swimmingDistanceKm}</strong> swam so far this
+          year
+          <br />
+          <span className="text-xs">
+            {activity?.year.swimmingDistancePercentageChange} change on the same
+            time last year
+          </span>
         </Tile>
       </Wall>
     </EmbeddedWrapper>
--- packages/website/src/activity/activityDataProvider.ts
+++ packages/website/src/activity/activityDataProvider.ts
@@ -3,31 +3,60 @@ import { DataProvider } from "@mattb.tech/data-fetching";
 import { useQuery } from "@apollo/client";
 import formatISO from "date-fns/formatISO";
 import startOfYear from "date-fns/startOfYear";
-import endOfYear from "date-fns/endOfYear";
 import { ActivityQuery } from "generated/graphql";
+import subDays from "date-fns/subDays";
+import { subYears } from "date-fns";
+
+const baseDate = subDays(new Date(), 1);
 
 const QUERY = gql`
-  query Activity($startDate: Date!, $endDate: Date!) {
-    activity(startDate: $startDate, endDate: $endDate) {
-      walkingRunningDistance {
-        km
-      }
-      swimmingDistance {
-        km
-      }
+  query Activity(
+    $startOfYear: Date!
+    $today: Date!
+    $thirtyDaysAgo: Date!
+    $startOfPreviousYear: Date!
+    $todayLastYear: Date!
+    $thirtyDaysAgoLastYear: Date!
+  ) {
+    thisYear: activity(startDate: $startOfYear, endDate: $today) {
+      ...ActivityData
+    }
+    lastYear: activity(
+      startDate: $startOfPreviousYear
+      endDate: $todayLastYear
+    ) {
+      ...ActivityData
+    }
+    trailing30Days: activity(startDate: $thirtyDaysAgo, endDate: $today) {
+      ...ActivityData
+    }
+    lastYearTrailing30Days: activity(
+      startDate: $thirtyDaysAgoLastYear
+      endDate: $todayLastYear
+    ) {
+      ...ActivityData
+    }
+  }
+
+  fragment ActivityData on Activity {
+    walkingRunningDistance {
+      km
+    }
+    swimmingDistance {
+      km
     }
   }
 `;
 
-const activityDataProvider: DataProvider<never, ActivityQuery> = async (
-  _: never,
-  { client }
-) => {
+const activityDataProvider: DataProvider<
+  never,
+  ReturnType<typeof transformData>
+> = async (_: never, { client }) => {
   const result = await client.query<ActivityQuery>({
     query: QUERY,
     variables: buildVariables(),
   });
-  return result.data;
+  return transformData(result.data);
 };
 
 export default activityDataProvider;
@@ -39,14 +68,69 @@ export function useActivity() {
   });
   return {
     loading,
-    activity: data?.activity,
+    activity: data ? transformData(data) : undefined,
   };
 }
 
-function buildVariables() {
-  const now = new Date();
+function transformData(data: ActivityQuery) {
   return {
-    startDate: formatISO(startOfYear(now), { representation: "date" }),
-    endDate: formatISO(endOfYear(now), { representation: "date" }),
+    year: {
+      walkingRunningKm: formatKm(data.thisYear.walkingRunningDistance.km),
+      walkingRunningPercentageChange: percentageChange(
+        data.thisYear.walkingRunningDistance.km,
+        data.lastYear.walkingRunningDistance.km
+      ),
+      swimmingDistanceKm: formatKm(data.thisYear.swimmingDistance.km),
+      swimmingDistancePercentageChange: percentageChange(
+        data.thisYear.swimmingDistance.km,
+        data.lastYear.swimmingDistance.km
+      ),
+    },
+    trailing30: {
+      walkingRunningKm: formatKm(data.trailing30Days.walkingRunningDistance.km),
+      walkingRunningPercentageChange: percentageChange(
+        data.trailing30Days.walkingRunningDistance.km,
+        data.lastYearTrailing30Days.walkingRunningDistance.km
+      ),
+      swimmingDistanceKm: formatKm(data.trailing30Days.swimmingDistance.km),
+      swimmingDistancePercentageChange: percentageChange(
+        data.trailing30Days.swimmingDistance.km,
+        data.lastYearTrailing30Days.swimmingDistance.km
+      ),
+    },
+  };
+}
+
+function formatKm(value: number): string {
+  return `${value.toFixed(2)}km`;
+}
+
+function percentageChange(newValue: number, oldValue: number): string {
+  if (oldValue === 0) {
+    return "↑∞%";
+  }
+  const percentage = ((newValue - oldValue) / oldValue) * 100;
+  if (percentage < 0) {
+    return `↓${(percentage * -1).toFixed(1)}%`;
+  }
+  return `↑${percentage.toFixed(1)}%`;
+}
+
+function buildVariables() {
+  return {
+    startOfYear: formatISO(startOfYear(baseDate), { representation: "date" }),
+    today: formatISO(baseDate, { representation: "date" }),
+    thirtyDaysAgo: formatISO(subDays(baseDate, 30), {
+      representation: "date",
+    }),
+    startOfPreviousYear: formatISO(startOfYear(subYears(baseDate, 1)), {
+      representation: "date",
+    }),
+    todayLastYear: formatISO(subYears(baseDate, 1), {
+      representation: "date",
+    }),
+    thirtyDaysAgoLastYear: formatISO(subDays(subYears(baseDate, 1), 30), {
+      representation: "date",
+    }),
   };
 }
