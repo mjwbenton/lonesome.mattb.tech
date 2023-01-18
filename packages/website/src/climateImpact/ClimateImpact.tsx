a005acaca1b9b1bc986f216adc3d6d5251ba32b7
Matthew Benton <m@mattb.tech>
2023-01-18 20:57:50 +0000

    Add icons to data on index page

--- packages/website/src/climateImpact/ClimateImpact.tsx
+++ packages/website/src/climateImpact/ClimateImpact.tsx
@@ -2,6 +2,8 @@ import EmbeddedWrapper from "component/EmbeddedWrapper";
 import { TopRightSpinner } from "component/Spinner";
 import Tile, { Wall } from "component/Tile";
 import { useClimateImpact } from "./climateImpactDataProvider";
+import { RiLeafLine, RiBuilding3Line } from "react-icons/ri";
+import Icon from "component/Icon";
 
 export default function Activity() {
   const { loading, climateImpact } = useClimateImpact();
@@ -11,9 +13,11 @@ export default function Activity() {
       <TopRightSpinner show={loading} />
       <Wall>
         <Tile>
+          <Icon component={RiLeafLine} />
           <strong>{climateImpact?.trees}</strong> Trees Planted
         </Tile>
         <Tile>
+          <Icon component={RiBuilding3Line} />
           <strong>{climateImpact?.carbonOffsetTonnes}t</strong> of carbon
           emissions avoided
         </Tile>
--- packages/website/src/codeContributions/CodeContributions.tsx
+++ packages/website/src/codeContributions/CodeContributions.tsx
@@ -2,6 +2,8 @@ import EmbeddedWrapper from "component/EmbeddedWrapper";
 import { TopRightSpinner } from "component/Spinner";
 import Tile, { Wall } from "component/Tile";
 import { useCodeContributions } from "./codeContributionsDataProvider";
+import { RiGitCommitLine, RiGitRepositoryLine } from "react-icons/ri";
+import Icon from "component/Icon";
 
 export default function CodeContributions() {
   const { loading, codeContributions } = useCodeContributions();
@@ -11,9 +13,11 @@ export default function CodeContributions() {
       <TopRightSpinner show={loading} />
       <Wall>
         <Tile>
+          <Icon component={RiGitCommitLine} />
           <strong>{codeContributions?.commits}</strong> Commits
         </Tile>
         <Tile>
+          <Icon component={RiGitRepositoryLine} />
           <strong>{codeContributions?.repositoriesCommittedTo}</strong>{" "}
           repositories committed to
         </Tile>
