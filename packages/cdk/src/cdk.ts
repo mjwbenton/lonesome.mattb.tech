24557735850c48fc68c6b6a3be58e7b04d86916b
Matthew Benton <m@mattb.tech>
2023-02-16 21:32:18 +0000

    Setup OpenIDConnect provider for github actions

--- packages/cdk/src/cdk.ts
+++ packages/cdk/src/cdk.ts
@@ -1,5 +1,7 @@
 import * as cdk from "aws-cdk-lib";
-import { LonesomeWebsite } from "./stack";
+import { LonesomeCi } from "./LonesomeCi";
+import { LonesomeWebsite } from "./LonesomeWebsite";
 
 const app = new cdk.App();
 new LonesomeWebsite(app, "LonesomeWebsite");
+new LonesomeCi(app, "LonesomeCi");
