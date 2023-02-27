73ca2cefc8961d6ddccaa5496599250bff97c8bb
Matthew Benton <m@mattb.tech>
2023-02-27 19:56:05 +0000

    Remove LonesomeCi stack to use account stack

--- packages/cdk/src/cdk.ts
+++ packages/cdk/src/cdk.ts
@@ -1,7 +1,5 @@
 import * as cdk from "aws-cdk-lib";
-import { LonesomeCi } from "./LonesomeCi";
 import { LonesomeWebsite } from "./LonesomeWebsite";
 
 const app = new cdk.App();
 new LonesomeWebsite(app, "LonesomeWebsite");
-new LonesomeCi(app, "LonesomeCi");
