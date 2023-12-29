378ec4ad89c9374424a5a898376a59f4f41123b4
Matthew Benton <m@mattb.tech>
2023-12-29 16:19:55 +0000

    Add lazyload video

--- packages/website/src/recordings/Recordings.tsx
+++ packages/website/src/recordings/Recordings.tsx
@@ -2,6 +2,10 @@ import EmbeddedWrapper from "component/EmbeddedWrapper";
 import { TopRightSpinner } from "component/Spinner";
 import { useRecordings } from "./recordingsDataProvider";
 import Infoline from "component/Infoline";
+import LazyLoad from "react-lazyload";
+
+const VIDEO_CLASSNAMES =
+  "aspect-square w-full bg-light-1 dark:bg-dark-1 border-2 border-light-1 dark:border-dark-1";
 
 export default function Recordings() {
   const { loading, recordings } = useRecordings();
@@ -14,17 +18,29 @@ export default function Recordings() {
     <EmbeddedWrapper>
       <TopRightSpinner show={loading} />
       <div className="space-y-16">
-        {recordings?.map((recording) => (
-          <div key={recording.url}>
-            <video
-              controls
-              className="aspect-square w-full bg-light-1 dark:bg-dark-1 border-2 border-light-1 dark:border-dark-1"
-            >
+        {recordings?.map((recording, i) => {
+          const video = (
+            <video controls className={VIDEO_CLASSNAMES}>
               <source src={recording.url} type="video/mp4" />
             </video>
-            <Infoline>{recording.name}</Infoline>
-          </div>
-        ))}
+          );
+          return (
+            <div key={recording.url}>
+              {i !== 0 ? (
+                <LazyLoad
+                  once
+                  offset={200}
+                  placeholder={<div className={VIDEO_CLASSNAMES} />}
+                >
+                  {video}
+                </LazyLoad>
+              ) : (
+                video
+              )}
+              <Infoline>{recording.name}</Infoline>
+            </div>
+          );
+        })}
       </div>
     </EmbeddedWrapper>
   );
