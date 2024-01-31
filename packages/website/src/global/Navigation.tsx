6bc07693bfa5e89c7b818b111b137d7e7f785c47
Matthew Benton <m@mattb.tech>
2024-01-31 17:52:12 +0000

    Fix prettier

--- packages/website/src/global/Navigation.tsx
+++ packages/website/src/global/Navigation.tsx
@@ -15,14 +15,13 @@ export default function Navigation() {
   return (
     <nav className="p-4 border-t-4 bg-light-1 border-accent-light dark:border-accent-dark dark:bg-dark-1 space-y-4 md:space-y-0 md:divide-x divide-light-2 dark:divide-gray-700">
       {LINKS.map((entry) => (
-        (<Link
+        <Link
           href={entry.slug}
           key={entry.slug}
-          className="block w-full cursor-pointer text-lg text-center font-normal md:text-left md:w-auto md:inline-block md:pr-4 md:pl-4 first:pl-0 last:pr-0">
-
+          className="block w-full cursor-pointer text-lg text-center font-normal md:text-left md:w-auto md:inline-block md:pr-4 md:pl-4 first:pl-0 last:pr-0"
+        >
           {entry.display}
-
-        </Link>)
+        </Link>
       ))}
     </nav>
   );
