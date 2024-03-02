ea8949cba3dfd45463a8c84a4ec3ab90b240b5d5
Matthew Benton <m@mattb.tech>
2024-03-02 14:15:22 +0000

    Fix info line for overflowing

--- packages/website/src/component/Infoline.tsx
+++ packages/website/src/component/Infoline.tsx
@@ -9,10 +9,10 @@ const Infoline = ({
   externalLinkText?: string;
   children: any;
 }) => (
-  <div className="flex p-2 text-xs bg-light-1 dark:bg-dark-1">
-    <div className="flex-1 inline">{children}</div>
+  <div className="flex p-2 text-xs bg-light-1 dark:bg-dark-1 max-w-[100vw]">
+    <div className="flex-1 min-w-0">{children}</div>
     {externalLinkUrl ? (
-      <a className="external-link" href={externalLinkUrl}>
+      <a className="flex-none external-link" href={externalLinkUrl}>
         {externalLinkText}
       </a>
     ) : null}
