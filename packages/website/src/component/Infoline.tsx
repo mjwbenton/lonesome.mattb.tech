aab4a662484f71347034da70630fa91ca305a5d5
Matthew Benton <m@mattb.tech>
2024-07-24 21:19:58 +0100

    Only show share button when not in share mode, increase touch area

--- packages/website/src/component/Infoline.tsx
+++ packages/website/src/component/Infoline.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Icon from "./Icon";
 import { Share } from "react-feather";
+import { isShareMode } from "utils/isShareMode";
 
 const Infoline = ({
   externalLinkUrl,
@@ -13,15 +14,18 @@ const Infoline = ({
   shareUrl?: string;
   children: any;
 }) => (
-  <div className="flex p-2 text-xs bg-light-1 dark:bg-dark-1 max-w-[100vw]">
-    <div className="flex-1 min-w-0">{children}</div>
-    {shareUrl ? (
-      <a className="flex-none pr-2" href={shareUrl}>
+  <div className="flex text-xs bg-light-1 dark:bg-dark-1 max-w-[100vw]">
+    <div className="p-2 flex-1 min-w-0">{children}</div>
+    {shareUrl && !isShareMode() ? (
+      <a className="block flex-none p-2 min-w-14 text-center" href={shareUrl}>
         Sh <Icon component={Share} size="xsmall" />
       </a>
     ) : null}
     {externalLinkUrl ? (
-      <a className="flex-none external-link" href={externalLinkUrl}>
+      <a
+        className="block flex-none p-2 min-w-14 text-center external-link"
+        href={externalLinkUrl}
+      >
         {externalLinkText}
       </a>
     ) : null}
