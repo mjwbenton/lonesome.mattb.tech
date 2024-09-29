f7c1503855922bbc970f11d4f78846eb824b165d
Matthew Benton <m@mattb.tech>
2024-09-29 20:34:07 +0100

    Gear page update

--- packages/website/src/component/Expander.tsx
+++ packages/website/src/component/Expander.tsx
@@ -3,7 +3,7 @@ import {
   DisclosureContent,
   useDisclosureStore,
 } from "@ariakit/react";
-import { ChevronsDown as DownIcon, ChevronsUp as UpIcon } from "react-feather";
+import { ChevronsDown } from "react-feather";
 
 export interface ExpanderProps {
   text: string;
@@ -12,11 +12,10 @@ export interface ExpanderProps {
 
 export default function Expander({ text, children }: ExpanderProps) {
   const disclosure = useDisclosureStore();
-  const Icon = disclosure.getState().open ? UpIcon : DownIcon;
   return (
     <>
-      <Disclosure store={disclosure}>
-        <Icon size={14} className="inline-block" />{" "}
+      <Disclosure className="block" store={disclosure}>
+        <ChevronsDown size={14} className="inline-block" />{" "}
         <span className="underline">{text}</span>
       </Disclosure>
       <DisclosureContent store={disclosure}>{children}</DisclosureContent>
