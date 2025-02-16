de7800a03ca23a256e9cce6763d55bfa51f83c07
Matthew Benton <m@mattb.tech>
2025-02-16 18:21:44 +0000

    Attempt to fix pa11y (3)

--- packages/website-audits/pa11y.js
+++ packages/website-audits/pa11y.js
@@ -4,6 +4,9 @@ module.exports = {
   defaults: {
     runners: ["axe", "htmlcs"],
     hideElements: ".pa11y-ignore-element",
+    chromeLaunchConfig: {
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    },
   },
   urls,
 };
