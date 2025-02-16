ab69437e47bbaaec0eb674e4be453a946d3bd9af
Matthew Benton <m@mattb.tech>
2025-02-16 16:08:27 +0000

    Revert "Use a single query for the year review"
    This reverts commit d6d096bede9873edb5c1739da197e99a9cff8106.

--- packages/website/src/yearReview/yearReviewDataProvider.ts
+++ packages/website/src/yearReview/yearReviewDataProvider.ts
@@ -14,30 +14,12 @@ const QUERY = gql`
     $endDateTime: DateTime!
     $startDate: Date!
     $endDate: Date!
-    $previousStartDateTime: DateTime!
-    $previousEndDateTime: DateTime!
-    $previousStartDate: Date!
-    $previousEndDate: Date!
     $tvSeriesLimit: Int!
   ) {
-    photos: photos(first: 0, startDate: $startDateTime, endDate: $endDateTime) {
-      total
-    }
-    previousPhotos: photos(
-      first: 0
-      startDate: $previousStartDateTime
-      endDate: $previousEndDateTime
-    ) {
+    photos(first: 0, startDate: $startDateTime, endDate: $endDateTime) {
       total
     }
     commitStats(startDate: $startDateTime, endDate: $endDateTime) {
-      commits
-      repositoriesCommittedTo
-    }
-    previousCommitStats: commitStats(
-      startDate: $previousStartDateTime
-      endDate: $previousEndDateTime
-    ) {
       commits
       repositoriesCommittedTo
     }
@@ -49,29 +31,11 @@ const QUERY = gql`
     ) {
       total
     }
-    previousBooks: books(
-      first: 0
-      startDate: $previousStartDateTime
-      endDate: $previousEndDateTime
-      sortBy: ADDED_AT
-    ) {
-      total
-    }
     readBooks: bookShelf(id: Read) {
       items(
         first: 0
         startDate: $startDateTime
         endDate: $endDateTime
-        sortBy: MOVED_AT
-      ) {
-        total
-      }
-    }
-    previousReadBooks: bookShelf(id: Read) {
-      items(
-        first: 0
-        startDate: $previousStartDateTime
-        endDate: $previousEndDateTime
         sortBy: MOVED_AT
       ) {
         total
@@ -87,28 +51,10 @@ const QUERY = gql`
         total
       }
     }
-    previousGaveUpBooks: bookShelf(id: DidNotFinish) {
-      items(
-        first: 0
-        startDate: $previousStartDateTime
-        endDate: $previousEndDateTime
-        sortBy: MOVED_AT
-      ) {
-        total
-      }
-    }
     movies(
       first: 0
       startDate: $startDateTime
       endDate: $endDateTime
-      sortBy: ADDED_AT
-    ) {
-      total
-    }
-    previousMovies: movies(
-      first: 0
-      startDate: $previousStartDateTime
-      endDate: $previousEndDateTime
       sortBy: ADDED_AT
     ) {
       total
@@ -121,29 +67,11 @@ const QUERY = gql`
     ) {
       total
     }
-    previousTvSeasons: tvSeasons(
-      first: 0
-      startDate: $previousStartDateTime
-      endDate: $previousEndDateTime
-      sortBy: ADDED_AT
-    ) {
-      total
-    }
     finishedTvSeasons: tvSeasonShelf(id: FinishedSeason) {
       items(
         first: 0
         startDate: $startDateTime
         endDate: $endDateTime
-        sortBy: MOVED_AT
-      ) {
-        total
-      }
-    }
-    previousFinishedTvSeasons: tvSeasonShelf(id: FinishedSeason) {
-      items(
-        first: 0
-        startDate: $previousStartDateTime
-        endDate: $previousEndDateTime
         sortBy: MOVED_AT
       ) {
         total
@@ -159,28 +87,10 @@ const QUERY = gql`
         total
       }
     }
-    previousGaveUpTvSeasons: tvSeasonShelf(id: GaveUp) {
-      items(
-        first: 0
-        startDate: $previousStartDateTime
-        endDate: $previousEndDateTime
-        sortBy: MOVED_AT
-      ) {
-        total
-      }
-    }
     videoGames(
       first: 0
       startDate: $startDateTime
       endDate: $endDateTime
-      sortBy: ADDED_AT
-    ) {
-      total
-    }
-    previousVideoGames: videoGames(
-      first: 0
-      startDate: $previousStartDateTime
-      endDate: $previousEndDateTime
       sortBy: ADDED_AT
     ) {
       total
@@ -195,31 +105,11 @@ const QUERY = gql`
         total
       }
     }
-    previousCompletedVideoGames: videoGameShelf(id: Completed) {
-      items(
-        first: 0
-        startDate: $previousStartDateTime
-        endDate: $previousEndDateTime
-        sortBy: MOVED_AT
-      ) {
-        total
-      }
-    }
     gaveUpVideoGames: videoGameShelf(id: GaveUp) {
       items(
         first: 0
         startDate: $startDateTime
         endDate: $endDateTime
-        sortBy: MOVED_AT
-      ) {
-        total
-      }
-    }
-    previousGaveUpVideoGames: videoGameShelf(id: GaveUp) {
-      items(
-        first: 0
-        startDate: $previousStartDateTime
-        endDate: $previousEndDateTime
         sortBy: MOVED_AT
       ) {
         total
@@ -241,22 +131,6 @@ const QUERY = gql`
         }
       }
     }
-    previousTvSeries: tvSeries(
-      first: $tvSeriesLimit
-      startDate: $previousStartDateTime
-      endDate: $previousEndDateTime
-      sortBy: MOVED_AT
-    ) {
-      total
-      items {
-        shelf {
-          id
-        }
-        seasons {
-          id
-        }
-      }
-    }
     activity(startDate: $startDate, endDate: $endDate) {
       swimmingDistance {
         km
@@ -265,25 +139,7 @@ const QUERY = gql`
         km
       }
     }
-    previousActivity: activity(
-      startDate: $previousStartDate
-      endDate: $previousEndDate
-    ) {
-      swimmingDistance {
-        km
-      }
-      walkingRunningDistance {
-        km
-      }
-    }
     plays(first: 0, startDate: $startDateTime, endDate: $endDateTime) {
-      total
-    }
-    previousPlays: plays(
-      first: 0
-      startDate: $previousStartDateTime
-      endDate: $previousEndDateTime
-    ) {
       total
     }
   }
@@ -328,165 +184,48 @@ export type YearData = {
   previousYear: YearCounts;
 };
 
-function transformResult(
+async function fetchForYear(
+  client: ApolloClient<NormalizedCacheObject>,
   year: number,
-  data: CountsBetweenDatesQuery,
-): YearData {
-  // Current year processing
-  if (data.tvSeries.total > TV_SERIES_LIMIT) {
-    throw new Error(
-      `Total TV series (${data.tvSeries.total}) exceeds fetch limit (${TV_SERIES_LIMIT})`,
-    );
+  overrideEndDate?: Date,
+): Promise<YearCounts> {
+  const result = await client.query<CountsBetweenDatesQuery>({
+    query: QUERY,
+    variables: buildYearQueryVariables(year, overrideEndDate),
+  });
+  const value = transformResult(year, result.data);
+  // Check the values are those expected – hacky test to see if any code changes
+  // accidentally cause these values to change after the fact
+  if (
+    !overrideEndDate &&
+    year in YEAR_VALUES &&
+    JSON.stringify(value) !== JSON.stringify(YEAR_VALUES[year])
+  ) {
+    console.log(JSON.stringify(value, null, 2));
+    throw new Error(`Year values for ${year} have changed unexpectedly`);
   }
-
-  const currentTvSeriesWithoutSeasons = data.tvSeries.items.filter(
-    ({ seasons }) => seasons.length === 0,
-  );
-  const currentTvSeriesFinished = currentTvSeriesWithoutSeasons.filter(
-    ({ shelf }) => shelf.id === "Finished",
-  ).length;
-  const currentTvSeriesGaveUp = currentTvSeriesWithoutSeasons.filter(
-    ({ shelf }) => shelf.id === "GaveUp",
-  ).length;
-  const currentTvSeriesStarted = currentTvSeriesWithoutSeasons.filter(
-    ({ shelf }) => shelf.id === "Watching",
-  ).length;
-
-  // Previous year processing
-  if (data.previousTvSeries.total > TV_SERIES_LIMIT) {
-    throw new Error(
-      `Total TV series (${data.previousTvSeries.total}) exceeds fetch limit (${TV_SERIES_LIMIT})`,
-    );
-  }
-
-  const previousTvSeriesWithoutSeasons = data.previousTvSeries.items.filter(
-    ({ seasons }) => seasons.length === 0,
-  );
-  const previousTvSeriesFinished = previousTvSeriesWithoutSeasons.filter(
-    ({ shelf }) => shelf.id === "Finished",
-  ).length;
-  const previousTvSeriesGaveUp = previousTvSeriesWithoutSeasons.filter(
-    ({ shelf }) => shelf.id === "GaveUp",
-  ).length;
-  const previousTvSeriesStarted = previousTvSeriesWithoutSeasons.filter(
-    ({ shelf }) => shelf.id === "Watching",
-  ).length;
-
-  return {
-    year: {
-      year,
-      photos: data.photos.total,
-      commitStats: {
-        commits: data.commitStats.commits,
-        repositoriesCommittedTo: data.commitStats.repositoriesCommittedTo,
-      },
-      books: {
-        started: data.books.total,
-        finished: data.readBooks?.items.total ?? 0,
-        gaveUp: data.gaveUpBooks?.items.total ?? 0,
-      },
-      movies: {
-        watched: data.movies.total,
-      },
-      videoGames: {
-        started: data.videoGames.total,
-        completed: data.completedVideoGames?.items.total ?? 0,
-        gaveUp: data.gaveUpVideoGames?.items.total ?? 0,
-      },
-      tvSeasons: {
-        started: data.tvSeasons.total + currentTvSeriesStarted,
-        finished:
-          data.finishedTvSeasons?.items.total ?? 0 + currentTvSeriesFinished,
-        gaveUp: data.gaveUpTvSeasons?.items.total ?? 0 + currentTvSeriesGaveUp,
-      },
-      activity: {
-        walkingRunningDistance: Math.floor(
-          data.activity.walkingRunningDistance.km,
-        ),
-        swimmingDistance: Math.floor(data.activity.swimmingDistance.km),
-      },
-      tracks: {
-        listened: data.plays.total,
-      },
-    },
-    previousYear: {
-      year: year - 1,
-      photos: data.previousPhotos.total,
-      commitStats: {
-        commits: data.previousCommitStats.commits,
-        repositoriesCommittedTo:
-          data.previousCommitStats.repositoriesCommittedTo,
-      },
-      books: {
-        started: data.previousBooks.total,
-        finished: data.previousReadBooks?.items.total ?? 0,
-        gaveUp: data.previousGaveUpBooks?.items.total ?? 0,
-      },
-      movies: {
-        watched: data.previousMovies.total,
-      },
-      videoGames: {
-        started: data.previousVideoGames.total,
-        completed: data.previousCompletedVideoGames?.items.total ?? 0,
-        gaveUp: data.previousGaveUpVideoGames?.items.total ?? 0,
-      },
-      tvSeasons: {
-        started: data.previousTvSeasons.total + previousTvSeriesStarted,
-        finished:
-          data.previousFinishedTvSeasons?.items.total ??
-          0 + previousTvSeriesFinished,
-        gaveUp:
-          data.previousGaveUpTvSeasons?.items.total ??
-          0 + previousTvSeriesGaveUp,
-      },
-      activity: {
-        walkingRunningDistance: Math.floor(
-          data.previousActivity.walkingRunningDistance.km,
-        ),
-        swimmingDistance: Math.floor(data.previousActivity.swimmingDistance.km),
-      },
-      tracks: {
-        listened: data.previousPlays.total,
-      },
-    },
-  };
+  return value;
 }
 
 const yearReviewDataProvider: DataProvider<
   { filterYear: number },
   YearData
 > = async ({ filterYear }, { client }) => {
-  const overrideEndDate = isCurrentYear(filterYear) ? new Date() : undefined;
-
-  const result = await client.query<CountsBetweenDatesQuery>({
-    query: QUERY,
-    variables: buildYearQueryVariables(filterYear, overrideEndDate),
-  });
-
-  const value = transformResult(filterYear, result.data);
-
-  // Check the values are those expected
-  if (
-    !overrideEndDate &&
-    filterYear in YEAR_VALUES &&
-    JSON.stringify(value.year) !== JSON.stringify(YEAR_VALUES[filterYear])
-  ) {
-    console.log(JSON.stringify(value.year, null, 2));
-    throw new Error(`Year values for ${filterYear} have changed unexpectedly`);
+  if (isCurrentYear(filterYear)) {
+    return {
+      filterYear,
+      year: await fetchForYear(client, filterYear, new Date()),
+      previousYear: await fetchForYear(
+        client,
+        filterYear - 1,
+        subYears(new Date(), 1),
+      ),
+    };
   }
-  if (
-    !overrideEndDate &&
-    filterYear - 1 in YEAR_VALUES &&
-    JSON.stringify(value.previousYear) !==
-      JSON.stringify(YEAR_VALUES[filterYear - 1])
-  ) {
-    console.log(JSON.stringify(value.previousYear, null, 2));
-    throw new Error(
-      `Year values for ${filterYear - 1} have changed unexpectedly`,
-    );
-  }
-
-  return value;
+  return {
+    year: await fetchForYear(client, filterYear),
+    previousYear: await fetchForYear(client, filterYear - 1),
+  };
 };
 
 export default yearReviewDataProvider;
@@ -496,20 +235,32 @@ export default yearReviewDataProvider;
  * Skips fetching unless its the current year
  */
 export function useLiveYearReviewData(year: number) {
-  const { data, loading } = useQuery<CountsBetweenDatesQuery>(QUERY, {
-    variables: buildYearQueryVariables(
-      year,
-      isCurrentYear(year) ? new Date() : undefined,
-    ),
-    skip: !isCurrentYear(year),
-    fetchPolicy: "network-only",
-  });
+  const { data: currentYearData, loading: currentYearLoading } =
+    useQuery<CountsBetweenDatesQuery>(QUERY, {
+      variables: buildYearQueryVariables(
+        year,
+        isCurrentYear(year) ? new Date() : undefined,
+      ),
+      skip: !isCurrentYear(year),
+      fetchPolicy: "network-only",
+    });
+
+  const { data: previousYearData, loading: previousYearLoading } =
+    useQuery<CountsBetweenDatesQuery>(QUERY, {
+      variables: buildYearQueryVariables(
+        year - 1,
+        isCurrentYear(year) ? subYears(new Date(), 1) : undefined,
+      ),
+      skip: !isCurrentYear(year),
+      fetchPolicy: "network-only",
+    });
 
   return {
-    loading,
-    ...(data
-      ? transformResult(year, data)
-      : { year: undefined, previousYear: undefined }),
+    loading: currentYearLoading || previousYearLoading,
+    year: currentYearData ? transformResult(year, currentYearData) : undefined,
+    previousYear: previousYearData
+      ? transformResult(year - 1, previousYearData)
+      : undefined,
   };
 }
 
@@ -555,14 +306,64 @@ function buildYearQueryVariables(
     endDate: overrideEndDate
       ? formatISO(overrideEndDate, { representation: "date" })
       : `${year}-12-31`,
-    previousStartDateTime: `${year - 1}-01-01T00:00:00Z`,
-    previousEndDateTime: overrideEndDate
-      ? formatISO(subYears(overrideEndDate, 1))
-      : `${year - 1}-12-31T23:59:59Z`,
-    previousStartDate: `${year - 1}-01-01`,
-    previousEndDate: overrideEndDate
-      ? formatISO(subYears(overrideEndDate, 1), { representation: "date" })
-      : `${year - 1}-12-31`,
     tvSeriesLimit: TV_SERIES_LIMIT,
+  };
+}
+
+function transformResult(
+  year: number,
+  data: CountsBetweenDatesQuery,
+): YearCounts {
+  if (data.tvSeries.total > TV_SERIES_LIMIT) {
+    throw new Error(
+      `Total TV series (${data.tvSeries.total}) exceeds fetch limit (${TV_SERIES_LIMIT})`,
+    );
+  }
+  const tvSeriesWithoutSeasons = data.tvSeries.items.filter(
+    ({ seasons }) => seasons.length === 0,
+  );
+  const tvSeriesFinished = tvSeriesWithoutSeasons.filter(
+    ({ shelf }) => shelf.id === "Finished",
+  ).length;
+  const tvSeriesGaveUp = tvSeriesWithoutSeasons.filter(
+    ({ shelf }) => shelf.id === "GaveUp",
+  ).length;
+  const tvSeriesStarted = tvSeriesWithoutSeasons.filter(
+    ({ shelf }) => shelf.id === "Watching",
+  ).length;
+  return {
+    year,
+    photos: data.photos.total,
+    commitStats: {
+      commits: data.commitStats.commits,
+      repositoriesCommittedTo: data.commitStats.repositoriesCommittedTo,
+    },
+    books: {
+      started: data.books.total,
+      finished: data.readBooks?.items.total ?? 0,
+      gaveUp: data.gaveUpBooks?.items.total ?? 0,
+    },
+    movies: {
+      watched: data.movies.total,
+    },
+    videoGames: {
+      started: data.videoGames.total,
+      completed: data.completedVideoGames?.items.total ?? 0,
+      gaveUp: data.gaveUpVideoGames?.items.total ?? 0,
+    },
+    tvSeasons: {
+      started: data.tvSeasons.total + tvSeriesStarted,
+      finished: data.finishedTvSeasons?.items.total ?? 0 + tvSeriesFinished,
+      gaveUp: data.gaveUpTvSeasons?.items.total ?? 0 + tvSeriesGaveUp,
+    },
+    activity: {
+      walkingRunningDistance: Math.floor(
+        data.activity.walkingRunningDistance.km,
+      ),
+      swimmingDistance: Math.floor(data.activity.swimmingDistance.km),
+    },
+    tracks: {
+      listened: data.plays.total,
+    },
   };
 }
