27f3ceda08273d23dd125e710edc70f256d52eb4
Matthew Benton <m@mattb.tech>
2025-07-26 18:53:20 +0100

    Try and fix cypress tests

--- packages/cypress-tests/cypress/support/e2e.js
+++ packages/cypress-tests/cypress/support/e2e.js
@@ -1,27 +1,18 @@
-// ***********************************************************
-// This example support/index.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
-// Import commands.js using ES2015 syntax:
-import "./commands";
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
-
-// Set test authentication header for all requests
 beforeEach(() => {
+  const authHeader = Cypress.env("TEST_AUTH_SECRET");
   cy.intercept("*", (req) => {
-    req.headers["x-lonesome-test-auth"] = Cypress.env("TEST_AUTH_SECRET");
+    req.headers["x-lonesome-test-auth"] = authHeader;
+  });
+});
+
+Cypress.Commands.overwrite("visit", (originalFn, url, options = {}) => {
+  const authHeader = Cypress.env("TEST_AUTH_SECRET");
+
+  return originalFn(url, {
+    ...options,
+    headers: {
+      ...options.headers,
+      "x-lonesome-test-auth": authHeader,
+    },
   });
 });
